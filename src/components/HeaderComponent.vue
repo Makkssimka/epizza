48036a772131b34343999f09180c1058044da007
Makkssimka <maxvolgstom@yandex.ru>
2020-03-01 23:08:25 +0400

    Шапка сайта. День 1

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="header-wrapper">
+        <div class="container">
+            <div class="logo-header">
+                <a href="/" title="ЁPizza">
+                    <img src="/images/epizza-logo.svg" alt="Ёpizza">
+                </a>
+            </div>
+            <div class="number-header">
+                <a href="tel:880045657890">8 800 4565-78-90</a>
+                <p>Звонок бесплатный</p>
+            </div>
+            <div class="bascket-header">
+                <a href="#" class="bascket-block">
+                    Корзина <span class="number">0</span>
+                </a>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
