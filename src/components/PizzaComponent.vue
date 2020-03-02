4dbe25c994488b9a2d8e9c0bfd32bcae3f6c7bd0
Makkssimka <maxvolgstom@yandex.ru>
2020-03-02 23:02:02 +0400

    закончил верстку внешнего вида списка

--- src/components/PizzaComponent.vue
+++ src/components/PizzaComponent.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="product-item">
-        {{ pizza.title }}
+        <a href="#" class="images-product">
+            <img :src="pizza.images.preview" alt="">
+        </a>
+        <div class="desc-product">
+            <p class="title-product">{{ pizza.title }}</p>
+            <p class="structure-product">{{ pizza.structure.join(', ') }}</p>
+        </div>
+        <div class="price-product-wrapper">
+            <div class="price-broduct">от {{ pizza.price.s }} &#8381;</div>
+            <a href="#" class="btn btn-product">Выбрать</a>
+        </div>
     </div>
 </template>
 
--- src/module/product.js
+++ src/module/product.js
@@ -66,6 +66,74 @@ const pizzaArray = [
             bold: '/images/pizza/piper-bold.jpeg',
             thin: '/images/pizza/piper-thin.jpeg'
         }
+    },
+    {
+        id: 5,
+        title: 'Пепперони Фреш с томатами',
+        price: {
+            s: 345,
+            m: 515,
+            mt: 515,
+            l: 685,
+            lt: 685
+        },
+        structure: ['Пикантная пепперони', 'томатный соус', 'моцарелла', 'томаты'],
+        images: {
+            preview: '/images/pizza/piper-bold.jpeg',
+            bold: '/images/pizza/piper-bold.jpeg',
+            thin: '/images/pizza/piper-thin.jpeg'
+        }
+    },
+    {
+        id: 6,
+        title: 'Пепперони Фреш с томатами',
+        price: {
+            s: 345,
+            m: 515,
+            mt: 515,
+            l: 685,
+            lt: 685
+        },
+        structure: ['Пикантная пепперони', 'томатный соус', 'моцарелла', 'томаты'],
+        images: {
+            preview: '/images/pizza/piper-bold.jpeg',
+            bold: '/images/pizza/piper-bold.jpeg',
+            thin: '/images/pizza/piper-thin.jpeg'
+        }
+    },
+    {
+        id: 7,
+        title: 'Пепперони Фреш с томатами',
+        price: {
+            s: 345,
+            m: 515,
+            mt: 515,
+            l: 685,
+            lt: 685
+        },
+        structure: ['Пикантная пепперони', 'томатный соус', 'моцарелла', 'томаты'],
+        images: {
+            preview: '/images/pizza/piper-bold.jpeg',
+            bold: '/images/pizza/piper-bold.jpeg',
+            thin: '/images/pizza/piper-thin.jpeg'
+        }
+    },
+    {
+        id: 8,
+        title: 'Пепперони Фреш с томатами',
+        price: {
+            s: 345,
+            m: 515,
+            mt: 515,
+            l: 685,
+            lt: 685
+        },
+        structure: ['Пикантная пепперони', 'томатный соус', 'моцарелла', 'томаты'],
+        images: {
+            preview: '/images/pizza/piper-bold.jpeg',
+            bold: '/images/pizza/piper-bold.jpeg',
+            thin: '/images/pizza/piper-thin.jpeg'
+        }
     }
 ];
 
